01c84f84e837917da30d99fcdff289b26e36a5c4
MiniOrange1 <lucas.cunha.souza@escola.pr.gov.br>
2026-09-21 11:17:22 +0000

    aula 4

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-
+const botaoJogarNovamente = document.querySelector(".novamente-btn");
 
 let atual = 0;
 let perguntaAtual;
@@ -45,6 +45,13 @@ function mostraResultado() {
   caixaPerguntas.textContent = "Em 2067...";
   textoResultado.textContent = historiaFinal;
   caixaAlternativas.textContent = "";
+  botaoJogarNovamente.addEventListener("click", jogaNovamente());
+}
+
+function jogaNovamente(){
+        atual = 0;
+        historiaFinal = "";
+        mostraPergunta();
 }
 
 mostraPergunta();
