74e2ac93611c66b585ad5fb1285e544525393ed8
MiniOrange1 <lucas.cunha.souza@escola.pr.gov.br>
2026-09-21 11:12:11 +0000

    aula 3

--- js/script.js
+++ js/script.js
@@ -42,7 +42,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-  caixaPerguntas.textContent = "Em 2049...";
+  caixaPerguntas.textContent = "Em 2067...";
   textoResultado.textContent = historiaFinal;
   caixaAlternativas.textContent = "";
 }
